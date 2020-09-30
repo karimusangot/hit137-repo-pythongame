62c9f62da6b5fdee08e53d621d6e9aa0e0dacb82
mjstacruz <71544460+mjstacruz@users.noreply.github.com>
2020-10-01 01:08:22 +0930

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -1,51 +1,80 @@
+import time
 import turtle
-wn = turtle.Screen()             # Set up the window and its attributes
-wn.bgcolor("grey")
+from turtle import Turtle
+from random import randint
+
+# window title
+window = turtle.Screen()
+window.title("Turtle Race")
+turtle.bgcolor("violet")
+turtle.color("white")
+turtle.speed(0)
+turtle.penup()
+turtle.setpos(-140, 200)
+turtle.write("Turtle Race", font=("Arial", 40, "bold"))
+turtle.penup()
+
+stamp_size = 20
+square_size = 15
+finish_line = 200
+
+turtle.color("black")
+turtle.shape("square")
+turtle.shapesize(square_size / stamp_size)
+turtle.penup()
+
+for i in range(20):
+    turtle.setpos(finish_line, (150 - (i * square_size * 1)))
+    turtle.stamp()
 
 
-turtmain = turtle.Turtle()           # create turtmain and set some attributes
-turtmain.color("purple")
-turtmain.pensize(2)
+turtle.hideturtle()
 
-turthidden = turtle.Turtle() #create turthidden and set some attributes
-turthidden.color("orange")
-        
-turtmain.left(90)   #begining of a cube drawing , main drawing
-turtmain.forward(200)                 
-turtmain.left(90)
-turtmain.forward(450)
-turtmain.left(90)
-turtmain.forward(200)
-turtmain.left(90) 
-turtmain.forward(450) 
-turtmain.left(30)  
-turtmain.forward(300) 
-turtmain.left(60)
-turtmain.forward(200)
-turtmain.left(90) 
-turtmain.forward(450)
-turtmain.left(30)  
-turtmain.forward(300) 
-turtmain.goto(0,200)
-turtmain.left (180)  
-turtmain.forward(300) 
-turtmain.penup()
-turtmain.home () #end of main drawing
+# TURTLE 1
+turtle1 = Turtle()
+turtle1.speed(0)
+turtle1.color("blue")
+turtle1.shape("turtle")
+turtle1.penup()
+turtle1.goto(-250, 100)
+turtle1.pendown()
 
 
-turthidden.penup()   #begining of drawing of hidden detaild of the main drawing
-turthidden.left(30)
-turthidden.forward(300)
-turthidden.pendown()               
-turthidden.left(150)
-turthidden.forward(450)
-turthidden.left(30)
-turthidden.forward(300)
-turthidden.penup()
-turthidden.left(180)
-turthidden.forward(300)
-turthidden.left(60)
-turthidden.pendown()
-turthidden.forward(200)  #end of the drawing of hidden details
+# TURTLE 2
+turtle2 = Turtle()
+turtle2.speed(0)
+turtle2.color("yellow")
+turtle2.shape("turtle")
+turtle2.penup()
+turtle2.goto(-250, 50)
+turtle2.pendown()
 
-wn.exitonclick()
+# TURTLE 3
+turtle3 = Turtle()
+turtle3.speed(0)
+turtle3.color("red")
+turtle3.shape("turtle")
+turtle3.penup()
+turtle3.goto(-250, 0)
+turtle3.pendown()
+
+
+# TURTLE 4
+turtle4 = Turtle()
+turtle4.speed(0)
+turtle4.color("white")
+turtle4.shape("turtle")
+turtle4.penup()
+turtle4.goto(-250, -50)
+turtle4.pendown()
+
+for i in range(145):
+    turtle1.forward(randint(1,5))
+    turtle2.forward(randint(1,5))
+    turtle3.forward(randint(1,5))
+    turtle4.forward(randint(1,5))
+
+
+turtle.exitonclick()
+
+turtle.mainloop()
