e16907388273ea949dcbb35f3b5179049dde6e10
Trust Karimusango <trustkarimusango@yahoo.com>
2020-10-18 17:12:01 +1100

    Updated version of our assignment

--- Assignment 2 Q2.py
+++ Assignment 2 Q2.py
@@ -1,17 +1,27 @@
+"""
 import pygame
+pygame.init()
 
+win = pygame.display.set_mode((600,400))
+
+pygame.display.set_caption ("Space invaders")
+
+bg=pygame.image.load('bg.png')
+Alien=[pygame.image.load('OIP.jpg')]
+Hero=[pygame.image.load('spaceship1.png')]
 
 class Game:
-    screen = None
+    screen = win
     aliens = []
     rockets = []
     lost = False
-
+ 
     def __init__(self, width, height):
-        pygame.init()
+        
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
+        
         self.clock = pygame.time.Clock()
         done = False
 
@@ -19,7 +29,15 @@ class Game:
         generator = Generator(self)
         rocket = None
 
-        while not done:
+        #while not done:
+        running=True    
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                       
+          
+
             if len(self.aliens) == 0:
                 self.displayText("VICTORY")
 
@@ -28,16 +46,22 @@ class Game:
                 hero.x -= 2 if hero.x > 20 else 0  
             elif pressed[pygame.K_RIGHT]: 
                 hero.x += 2 if hero.x < width - 20 else 0 
-
+            
+            
+ 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.lost:
-                    self.rockets.append(Rocket(self, hero.x, hero.y))
+                    
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE : #and not self.lost:
+                self.rockets.append(Rocket(self, hero.x, hero.y))
 
-            pygame.display.flip()
+            #pygame.display.flip()
+            pygame.display.update()
             self.clock.tick(60) #refresh rate
-            self.screen.fill((255, 255, 222))
+            self.screen.blit(bg,(0,0))
+            
+            #self.screen.fill((10, 30, 10))
 
             for alien in self.aliens:
                 alien.draw()
@@ -61,13 +85,14 @@ class Alien: #new alien class was created
         self.x = x
         self.game = game
         self.y = y
-        self.size = 30
+        self.size = 10
 
     def draw(self):
         pygame.draw.rect(self.game.screen,  
                          (104, 185, 70), 
                          pygame.Rect(self.x, self.y, self.size, self.size))
         self.y += 0.05 # pixel per frame
+        return Alien
 
     def checkCollision(self, game):
         for rocket in game.rockets:
@@ -89,6 +114,7 @@ class Hero: # The hero is the character that controlled by the player
         pygame.draw.rect(self.game.screen,
                          (210, 250, 251),
                          pygame.Rect(self.x, self.y, 8, 5))
+        return Hero
 
 
 class Generator: # This class will be able to create more enemies
@@ -110,10 +136,186 @@ class Rocket: # This is the weapon of the hero
 
     def draw(self):
         pygame.draw.rect(self.game.screen,  
-                         (254, 52, 110),  
+                         (200, 20, 110),  
                          pygame.Rect(self.x, self.y, 2, 4))
         self.y -= 2  
 
 
 if __name__ == '__main__':
     game = Game(600, 400)
+pygame.quit()
+quit()"""
+
+import pygame
+from pygame import mixer
+
+import math
+import random
+
+# Intialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((600, 600))
+
+# Background image
+background = pygame.image.load('bg.png')
+
+# background sound
+mixer.music.load("music_zapsplat_game_music_action_fast_paced_elelctro_sonic_011.mp3")
+mixer.music.play(-5)
+
+# Caption 
+pygame.display.set_caption("Space Invader")
+
+# Hero
+heroImage = pygame.image.load('spaceship1.png')
+heroX = 300
+heroY = 530
+heroX_change = 0
+
+# Alien creation
+alienImage = []
+alienX = []
+alienY = []
+alienX_change = []
+alienY_change = []
+num_of_aliens = 8
+# Creation of more aliens with alien attributes
+for i in range(num_of_aliens):
+    alienImage.append(pygame.image.load('UFO.gif'))
+    alienX.append(random.randint(0,580))
+    alienY.append(random.randint(50, 150))
+    alienX_change.append(2)
+    alienY_change.append(30)
+
+# Rocket creation 
+rocketImage = pygame.image.load('bullet_1.png')
+rocketX = 0
+rocketY = 430
+rocketX_change = 0
+rocketY_change = 10
+rocket_state = "ready"
+
+# Score initialisation and its font and size
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 20)
+textX = 10
+testY = 10
+
+# Game over font
+over_font = pygame.font.Font('freesansbold.ttf', 30)  
+
+# Function to show score on the screen and its attributes
+def show_score(x, y): 
+    score = font.render("Score : " + str(score_value), True, (255, 200, 210))
+    screen.blit(score, (x, y))
+
+# Function to display game over on the screen with its attributes
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (200, 220, 240))
+    screen.blit(over_text, (200, 250))
+
+# Function to place player on the screen
+def hero(x, y):
+    screen.blit(heroImage, (x, y))
+
+# Function to place the enermies on the screen
+def alien(x, y, i):
+    screen.blit(alienImage[i], (x, y))
+
+# Function to fire artillery
+def fire_rocket(x, y):
+    global rocket_state
+    rocket_state = "fire"
+    screen.blit(rocketImage, (x + 16, y + 10))
+
+# Function to check or determine collision
+def isCollision(alienX, alienY, rocketX, rocketY):
+    distance = math.sqrt(math.pow(alienX - rocketX, 2) + (math.pow(alienY - rocketY, 2)))
+    if distance < 20:
+        return True
+    else:
+        return False 
+
+
+# Main loop for game controls
+running = True
+while running:
+    hero(heroX, heroY)
+    show_score(textX, testY)
+    pygame.display.update()
+
+    # Place background image on the screen
+    screen.blit(background, (0, 0))  
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Check whether key pressed is right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                heroX_change = -5
+            if event.key == pygame.K_RIGHT:
+                heroX_change = 5
+            if event.key == pygame.K_SPACE:
+                if rocket_state is "ready":
+                    bulletSound = mixer.Sound("Artillery+2.wav")
+                    bulletSound.play()
+
+                    # Current x cordinate of the spaceship and the bullet
+                    rocketX = heroX
+                    fire_rocket(rocketX, rocketY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                heroX_change = 0
+
+    heroX += heroX_change
+    if heroX <= 0:
+        heroX = 0
+    elif heroX >= 560:
+        heroX = 535
+
+    # Movement of the aliens on the screen
+    for i in range(num_of_aliens):
+
+        # Game over conditions detection and actions to be taken
+        if alienY[i] > 460:
+            for j in range(num_of_aliens):
+                alienY[j] = 10000
+            game_over_text()
+            break
+
+        alienX[i] += alienX_change[i]
+        if alienX[i] <= 0:
+            alienX_change[i] = 1
+            alienY[i] +=alienY_change[i]
+        elif alienX[i] >=560:
+            alienX_change[i] = -1
+            alienY[i] +=alienY_change[i]
+
+        # Collision event detection and action to be taken 
+        collision = isCollision(alienX[i], alienY[i], rocketX, rocketY)
+        if collision:
+            explosionSound = mixer.Sound("Explosion+3.wav")
+            explosionSound.play()
+            rocketY = 460
+            rocket_state = "ready"
+            score_value += 1
+            alienX[i] = random.randint(0,570)
+            alienY[i] = random.randint(50, 150)
+
+        alien(alienX[i], alienY[i], i)
+
+    # Bullet Movement
+    if rocketY <= 0:
+        rocketY = 460
+        rocket_state = "ready"
+
+    if rocket_state is "fire":
+        fire_rocket(rocketX, rocketY)
+        rocketY -= rocketY_change
+
+pygame.quit()  
--- assignment 2.py
+++ assignment 2.py
@@ -13,10 +13,10 @@ t=turtle.Turtle()
 
 t.hideturtle()
 
-# TURTLE 1
+# TURTLE 1 created and starts drawing a five
 t1 = Turtle()
 t1.pensize(5)
-t1.speed(10)
+t1.speed(0)
 t1.color("blue","orange")
 t1.shape("turtle")
 t1.up()
@@ -53,15 +53,15 @@ t1.rt(89.7)
 t1.fd(195)
 t1.lt(90)
 t1.fd(52)
-t1.end_fill()
+t1.end_fill()# finishes drawing a five
 t1.up()
-t1.goto(-400, 370)
+t1.goto(-400, 370) #goes to the race starting point
 t1.rt(90.1)
 t1.pd()
 
-# TURTLE 2
+# TURTLE 2 created and goes to the race starting point
 t2 = Turtle()
-t2.speed(4)
+t2.speed(0)
 t2.color("orange")
 t2.shape("turtle")
 t2.up()
@@ -69,9 +69,9 @@ t2.goto(-400, 350)
 t2.pd()
 
 
-# TURTLE 3
+# TURTLE 3 created and goes to the race starting point
 t3 = Turtle()
-t3.speed(4)
+t3.speed(0)
 t3.color("light green")
 t3.shape("turtle")
 t3.up()
@@ -79,10 +79,10 @@ t3.goto(-400, 330)
 t3.pd()
 
 
-# TURTLE 4
+# TURTLE 4 created  and starts drawing a nine
 t4 = Turtle()
 t4.pensize(5)
-t4.speed(10)
+t4.speed(0)
 t4.color("purple","light green")
 t4.shape("turtle")
 t4.up()
@@ -125,15 +125,15 @@ for i in range(180):
     t4.rt(1)
 t4.fd(20)
 t4.up()
-t4.end_fill()
+t4.end_fill() # finishes drawing a nine
 t4.up()
-t4.goto(-400, 310)
+t4.goto(-400, 310) #goes to the race starting point
 t4.rt(93)
 t4.pd()
 
-# TURTLE 5
+# TURTLE 5 created and goes to the race starting point
 t5 = Turtle()
-t5.speed(4)
+t5.speed(0)
 t5.pensize(5)
 t5.color("purple")
 t5.shape("turtle")
@@ -142,9 +142,9 @@ t5.goto(340, 370)
 t5.pd()
 t5.rt(90)
 
-# TURTLE 6
+# TURTLE 6 created and goes to the race starting point
 t6 = Turtle()
-t6.speed(4)
+t6.speed(0)
 t6.color("light green")
 t6.shape("turtle")
 t6.up()
@@ -152,9 +152,9 @@ t6.goto(360, 370)
 t6.pd()
 t6.rt(90)
 
-# TURTLE 7
+# TURTLE 7 created and goes to the race starting point
 t7 = Turtle()
-t7.speed(4)
+t7.speed(0)
 t7.color("orange")
 t7.shape("turtle")
 t7.up()
@@ -162,9 +162,9 @@ t7.goto(380, 370)
 t7.pd()
 t7.rt(90)
 
-# TURTLE 8
+# TURTLE 8 created and goes to the race starting point
 t8 = Turtle()
-t8.speed(4)
+t8.speed(0)
 t8.pensize(5)
 t8.color("blue")
 t8.shape("turtle")
@@ -173,9 +173,9 @@ t8.goto(400, 370)
 t8.pd()
 t8.rt(90)
 
-# TURTLE 9
+# TURTLE 9 created and goes to the race starting point
 t9 = Turtle()
-t9.speed(4)
+t9.speed(0)
 t9.pensize(5)
 t9.color("purple")
 t9.shape("turtle")
@@ -184,9 +184,9 @@ t9.goto(400,-300)
 t9.pd()
 t9.rt(180)
 
-# TURTLE 10
+# TURTLE 10 created and goes to the race starting point
 t10 = Turtle()
-t10.speed(4)
+t10.speed(0)
 t10.color("light green")
 t10.shape("turtle")
 t10.up()
@@ -194,9 +194,9 @@ t10.goto(400,-320)
 t10.pd()
 t10.rt(180)
 
-# TURTLE 2
+# TURTLE 11 created and goes to the race starting point
 t11 = Turtle()
-t11.speed(4)
+t11.speed(0)
 t11.color("orange")
 t11.shape("turtle")
 t11.up()
@@ -204,9 +204,9 @@ t11.goto(400, -340)
 t11.pd()
 t11.rt(180)
 
-# TURTLE 12
+# TURTLE 12 created and goes to the race starting point
 t12 = Turtle()
-t12.speed(4)
+t12.speed(0)
 t12.pensize(5)
 t12.color("blue")
 t12.shape("turtle")
@@ -215,9 +215,9 @@ t12.goto(400, -360)
 t12.pd()
 t12.rt(180)
 
-# TURTLE 13
+# TURTLE 13 created and goes to the race starting point
 t13 = Turtle()
-t13.speed(4)
+t13.speed(0)
 t13.pensize(5)
 t13.color("purple")
 t13.shape("turtle")
@@ -226,9 +226,9 @@ t13.goto(-340, -360)
 t13.pd()
 t13.lt(90)
 
-# TURTLE 14
+# TURTLE 14 created and goes to the race starting point
 t14 = Turtle()
-t14.speed(4)
+t14.speed(0)
 t14.color("light green")
 t14.shape("turtle")
 t14.up()
@@ -236,9 +236,9 @@ t14.goto(-360, -360)
 t14.pd()
 t14.lt(90)
 
-# TURTLE 15
+# TURTLE 15created and goes to the race starting point 
 t15 = Turtle()
-t15.speed(4)
+t15.speed(0)
 t15.color("orange")
 t15.shape("turtle")
 t15.up()
@@ -246,9 +246,9 @@ t15.goto(-380, -360)
 t15.pd()
 t15.lt(90)
 
-# TURTLE 16
+# TURTLE 16created and goes to the race starting point 
 t16 = Turtle()
-t16.speed(4)
+t16.speed(0)
 t16.pensize(5)
 t16.color("blue")
 t16.shape("turtle")
@@ -257,25 +257,24 @@ t16.goto(-400, -360)
 t16.pd()
 t16.lt(90)
 
-
-
-for i in range(270):
-    t1.fd(randint(1,5))
-    t2.fd(randint(1,5))
-    t3.fd(randint(1,5))
-    t4.fd(randint(1,5))
-    t5.fd(randint(1,5))
-    t6.fd(randint(1,5))
-    t7.fd(randint(1,5))
-    t8.fd(randint(1,5))
-    t9.fd(randint(1,5))
-    t10.fd(randint(1,5))
-    t11.fd(randint(1,5))
-    t12.fd(randint(1,5))
-    t13.fd(randint(1,5))
-    t14.fd(randint(1,5))
-    t15.fd(randint(1,5))
-    t16.fd(randint(1,5))
-
+for i in range(200):  # The race starts as soon as all turltes have reached 
+                      # the starting point , distance per move is determined by a
+                      # random pick between 1 and 5 for all turtles
+    t1.fd(randint(5,10))
+    t2.fd(randint(5,10))
+    t3.fd(randint(5,10))
+    t4.fd(randint(5,10))
+    t5.fd(randint(5,10))
+    t6.fd(randint(5,10))
+    t7.fd(randint(5,10))
+    t8.fd(randint(5,10))
+    t9.fd(randint(5,10))
+    t10.fd(randint(5,10))
+    t11.fd(randint(5,10))
+    t12.fd(randint(5,10))
+    t13.fd(randint(5,10))
+    t14.fd(randint(5,10))
+    t15.fd(randint(5,10))
+    t16.fd(randint(5,10))
 window.exitonclick()
-window.mainloop()
+window.mainloop() # End of program
